d1e775226bb9d47777838cdf8beabe1a991d9898
Luci-Lawless <lu.gmmm@gmail.com>
2017-11-21 16:24:55 +0100

    Create role, admin and name columns for Users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,12 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, :all . # permissions for every user, even if not logged in
-    if user.present?  # additional permissions for logged in users (they can manage their posts)
-      can :manage, Post, user_id: user.id
-      if user.admin?  # additional permissions for administrators
+    if user.present?
+      can :read, :all
+      if user.admin?
         can :manage, :all
-        can :invite, :all
       end
     end
   end
